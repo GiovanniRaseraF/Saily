602c3e5c45fc27d3e7cc95126bc5b4675af6f8ec
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-09-03 07:38:12 +0200

    rec: record view

--- saily/lib/record/record_view.dart
+++ saily/lib/record/record_view.dart
@@ -1,4 +1,6 @@
 // Actual app
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:saily/routes/route_widget.dart';
@@ -24,15 +26,62 @@ class _RecordViewState extends State<RecordView> {
   _RecordViewState({required this.settingsController});
 
   SettingsController settingsController;
+  bool recording = false;
+
+  late Timer timeout;
+  int internalTime = 0;
+
+  // Add one second to the timer
+  void addOneSecond() {
+    internalTime++;
+    if (mounted) {
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return 
-    Container(
+    return Container(
       width: gCtxW() * 0.3,
       child: FloatingActionButton(
         onPressed: () {
-          print("Start recording");
+          if (recording) {
+            timeout.cancel();
+            print("STOP recording ${internalTime} s");
+            recording = false;
+
+            // create a dialog
+            showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => Dialog(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text('Save Route'),
+                      const SizedBox(height: 15),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Save'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+
+          } else {
+            internalTime = 0;
+            print("START recording");
+            recording = true;
+            timeout = Timer.periodic(Duration(seconds: 1), (t) {
+              addOneSecond();
+            });
+          }
         },
         backgroundColor: Colors.white,
         child: Row(children: [
@@ -44,9 +93,26 @@ class _RecordViewState extends State<RecordView> {
           MicrodividerWidgetd(height: 0),
           MicrodividerWidgetd(height: 0),
           MicrodividerWidgetd(height: 0),
-          Text("00:00")
+          TimerDisplay(seconds: internalTime)
         ]),
       ),
     );
+  }
+}
+
+// displays the intenal time as 00:00
+class TimerDisplay extends StatelessWidget {
+  TimerDisplay({required this.seconds});
+  int seconds;
+  @override
+  Widget build(BuildContext context) {
+    int mins = (seconds / 60).toInt();
+    int sec = (seconds % 60);
+
+    String minss = (mins < 10 ? "0" : "") + "${mins}";
+    String minunes = (mins < 10 ? "0" : "") + "${mins}";
+
+    String secs = (sec < 10 ? "0" : "") + "${sec}";
+    return Text("${minss}:${secs}");
   }
 }
