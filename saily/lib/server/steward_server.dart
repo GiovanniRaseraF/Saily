b05b3665abd0f168a5a170a9c4069549ee17f693
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-09-30 17:32:54 +0200

    server: steward server init

---
+++ saily/lib/server/steward_server.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:dart_either/src/dart_either.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart';
+import 'package:saily/datatypes/actuator_info.dart';
+import 'package:saily/datatypes/boat_info.dart';
+import 'package:saily/datatypes/electricmotor_info.dart';
+import 'package:saily/datatypes/endotermicmotor_info.dart';
+import 'package:saily/datatypes/general_info.dart';
+import 'package:saily/datatypes/highpowerbattery_info.dart';
+import 'package:saily/datatypes/nmea2000_info.dart';
+import 'package:saily/main.dart';
+import 'package:saily/server/server.dart';
+import 'package:saily/settings/settings_controller.dart';
+import 'package:saily/tracks/fake_data.dart';
+import 'package:saily/tracks/gpx_trips.dart';
+import 'package:saily/utils/utils.dart';
+
+class StewardServerInfo extends Server{
+  StewardServerInfo({required this.settingsController}){
+    runFetchProcess(interval: Duration(seconds: 10), callback: fetchProcess);
+  }
+
+  SettingsController settingsController;
+
+  void fetchProcess(Timer t){
+    print("Running fetch..");
+  }
+
+  Future<Either<InitError, String>> initServer() async {
+    return Either.left(InitError(why: "initServer is abstract"));
+  } 
+
+  @override
+  Future<Either<FetchError, ActuatorInfo>> fetchAcuatorInfo() {
+    // TODO: implement fetchAcuatorInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<FetchError, BoatInfo>> fetchBoatInfo() {
+    // TODO: implement fetchBoatInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<FetchError, ElectricmotorInfo>> fetchElectricmotorInfo() {
+    // TODO: implement fetchElectricmotorInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<FetchError, EndotermicmotorInfo>> fetchEndotermicmotorInfo() {
+    // TODO: implement fetchEndotermicmotorInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<FetchError, GeneralInfo>> fetchGeneralInfo() {
+    // TODO: implement fetchGeneralInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<FetchError, HighpowerbatteryInfo>> fetchHighpowerbatteryInfo() {
+    // TODO: implement fetchHighpowerbatteryInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<FetchError, VTGInfo>> fetchVTGInfo() {
+    // TODO: implement fetchVTGInfo
+    throw UnimplementedError();
+  }
+
+
+  @override
+  void runFetchProcess({required Duration interval, required void Function(Timer p1) callback}) {
+    fetch = Timer.periodic(interval, callback);
+  }
+
+  @override
+  void stopFetchProcess() {
+    fetch.cancel();
+  }
+}
