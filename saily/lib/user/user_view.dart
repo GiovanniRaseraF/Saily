5303e5813b9953fd6b17d38de3530bfa7db87388
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-10-02 17:38:38 +0200

    user: update user info page

--- saily/lib/user/user_view.dart
+++ saily/lib/user/user_view.dart
@@ -44,147 +44,89 @@ class _UserViewState extends State<UserView> {
 
       if (or == Orientation.portrait || true) {
         return Scaffold(
-        appBar: AppBar(
-          title: Text("User"),
-          backgroundColor: Colors.white,
-        ),
-        backgroundColor: Colors.white,
-          body: Center(
-            child: Column(
-              children: [
-                // Card(
-                //   elevation: 10,
-                //   child: TravelDestinationContent(destination: destinations[0])),
-              ],
+            appBar: AppBar(
+              title: Text("User"),
+              backgroundColor: Colors.white,
             ),
-          )
-        );
-      }else{
-        return Scaffold(
-          body: Row(
-            children: [],
-          ));
-      }});
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("User"),
-          backgroundColor: Colors.white,
-        ),
-        backgroundColor: Colors.white,
-        body: 
-        // Column(
-        //   mainAxisAlignment: MainAxisAlignment.start,
-        //   children: [
-            // FractionallySizedBox(
-            //     alignment: Alignment.topCenter,
-            //     child: Center(
-            //       child: Column(
-            //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //         children: [
-            //           Text("${currentUser!.username}"),
-            //           Text("${currentUser!.email}"),
-
-            //           FittedBox(
-            //               child: SizedBox(
-            //                 width: 200,
-            //                 child: FloatingActionButton(
-            //                     heroTag: "logout",
-            //                     child: Text(
-            //                       "Logout",
-            //                       style: TextStyle(color: Colors.white),
-            //                     ),
-            //                     backgroundColor: SailyBlue,
-            //                     elevation: 10,
-            //                     onPressed: () {
-            //                       settingsController.logout();
-            //                       Navigator.pop(context);
-            //                       onLogout();
-            //                       print("Log out from account");
-            //                     }),
-            //               ))
-            //         ],
-            //       ),
-            //     )),
-            // Divider(),
-            // SizedBox( child: Text("Boats"),),
-            Column(
-              children: [
-                SizedBox.expand(
-                  child: FractionallySizedBox(
-                    widthFactor: 1,
-                    heightFactor: 0.2,
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.red,
-                          width: 4,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                // SizedBox.expand(
-                //   child: FractionallySizedBox(
-                //     widthFactor: 1,
-                //     heightFactor: 0.6,
-                //     child: DecoratedBox(
-                //       decoration: BoxDecoration(
-                //         border: Border.all(
-                //           color: Colors.blue,
-                //           width: 4,
-                //         ),
-                //       ),
-                //     ),
-                //   ),
-                // ),
-              ]
-            ),
-            // FittedBox(
-            //   fit: BoxFit.contain,
-            //   child:SizedBox(
-            //     child: SizedBox(
-            //       child: Center(
-            //         child: SingleChildScrollView(
-            //           child: Column(
-            //             children : currentUser!.boats.map((b){
-            //               return BoatWidget(info: b, settingsController: settingsController, onDelete: (){setState(() {});},);
-            //             }).toList()
-            //           ),
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            
-        //   ],
-        // ),
-        floatingActionButton: SizedBox(
-                child: FloatingActionButton(
-                    heroTag: "add_new_boat",
-                    child: Icon(Icons.add, color: SailyAlmostWhite,),
-                    backgroundColor: SailyBlue,
-                    elevation: 10,
-                    onPressed: () {
-                      String name = "DefaultBoat";
-                      print("Add New Boat");
-                      Navigator.push(context, MaterialPageRoute(builder: (c){
-                        return TakePictureScreen(
-                          settingsController: settingsController,
-                          onQRCodeTaken: (scannedId){
-                            UserController.dialogCreator(context, scannedId, 
-                            (v){name = v;}, 
-                            (){
-                              BoatInfo newboat = BoatInfo(name: name, id: "0x" + name);
-                              settingsController.addNewBoat(newboat);
+            backgroundColor: Colors.white,
+            body: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Card(
+                        elevation: 10,
+                        color: Colors.white,
+                        child: UserWidget(
+                            userInfo: currentUser!,
+                            onLogout: () {
+                              settingsController.logout();
                               Navigator.pop(context);
-                              setState(() {});
-                              },
-                            (){Navigator.pop(context);});
-                          },
-                          );
-                      }));
-                    })),
-        );
+                              onLogout();
+                              print("Log out from account");
+                            })),
+                  )
+                ],
+              ),
+            ),
+            floatingActionButton: SizedBox(
+              child: FloatingActionButton(
+                  heroTag: "add_new_boat",
+                  child: Icon(
+                    Icons.add,
+                    color: SailyAlmostWhite,
+                  ),
+                  backgroundColor: SailyBlue,
+                  elevation: 10,
+                  onPressed: () {
+                    String name = "DefaultBoat";
+                    print("Add New Boat");
+                    Navigator.push(context, MaterialPageRoute(builder: (c) {
+                      return TakePictureScreen(
+                        settingsController: settingsController,
+                        onQRCodeTaken: (scannedId) {
+                          UserController.dialogCreator(context, scannedId, (v) {
+                            name = v;
+                          }, () {
+                            BoatInfo newboat =
+                                BoatInfo(name: name, id: "0x" + name);
+                            settingsController.addNewBoat(newboat);
+                            Navigator.pop(context);
+                            setState(() {});
+                          }, () {
+                            Navigator.pop(context);
+                          });
+                        },
+                      );
+                    }));
+                  }),
+            ));
+      } else {
+        return Scaffold(
+            body: Row(
+          children: [],
+        ));
+      }
+    });
+
+    // FittedBox(
+    //   fit: BoxFit.contain,
+    //   child:SizedBox(
+    //     child: SizedBox(
+    //       child: Center(
+    //         child: SingleChildScrollView(
+    //           child: Column(
+    //             children : currentUser!.boats.map((b){
+    //               return BoatWidget(info: b, settingsController: settingsController, onDelete: (){setState(() {});},);
+    //             }).toList()
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // ),
+
+    //   ],
+    // ),
   }
 }
